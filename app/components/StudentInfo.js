a4da69608d879f0d0c44ae0ea0d088f2ff503ec8
Verity <verity.boyd@edu.sait.ca>
2026-01-16 14:46:28 -0700

    Created week-2 folder, page.js inside week-2, StudentInfo component

---
+++ app/components/StudentInfo.js
@@ -0,0 +1,12 @@
+export default function StudentInfo() {
+  return (
+    <header>
+      <h2>Verity Boyd</h2>
+      <h2>
+        <Link href="https://github.com/verityboyd/cprg306-assignments">
+          GitHub Repo
+        </Link>
+      </h2>
+    </header>
+  );
+}
