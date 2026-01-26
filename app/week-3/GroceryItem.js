56cb5829431ea0adbe4326f668f1d53a444da270
Verity <verity.boyd@edu.sait.ca>
2026-01-26 15:39:40 -0700

    tweaked css styling

--- app/week-3/GroceryItem.js
+++ app/week-3/GroceryItem.js
@@ -2,7 +2,7 @@
 
 export default function GroceryItem({ name, quantity, category }) {
   return (
-    <li className="m-5 py-5 px-5 border rounded">
+    <li className="w-125 m-4 p-4 border rounded">
       {name}
       <div>Quantity: {quantity}</div>
       <div>Category: {category}</div>
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,10 +1,9 @@
-import GroceryItem from "./GroceryItem";
 import GroceryItemList from "./GroceryItemList";
 
 export default function Page() {
   return (
     <main className="flex flex-col items-center m-4">
-      <h1 className="font-bold text-xl">Shopping List</h1>
+      <h1 className="font-bold text-2xl">Shopping List</h1>
       <GroceryItemList />
     </main>
   );
