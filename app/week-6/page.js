b8edaf38d1c979bfd8b1a6933a952f1731d60fda
Verity <verity.boyd@edu.sait.ca>
2026-02-12 10:31:50 -0700

    added page component to week-6 page.js

---
+++ app/week-6/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main className="flex flex-col items-center m-4">
+      <h1 className="font-bold text-3xl">Add To Shopping List</h1>
+    </main>
+  );
+}
