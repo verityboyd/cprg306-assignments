f9d219b581fc572b4afd401028f29630a56ed65d
Verity <verity.boyd@edu.sait.ca>
2026-03-01 13:08:15 -0700

    week 7 assignment completed and tested

--- app/page.js
+++ app/page.js
@@ -22,6 +22,9 @@ export default function Page() {
             <li>
               <Link href="/week-6">Week 6</Link>
             </li>
+            <li>
+              <Link href="/week-7">Week 7</Link>
+            </li>
           </ul>
         </nav>
       </header>
---
+++ app/week-7/ItemList.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import Item from "./Item";
+
+export default function ItemList({ items }) {
+  const [sortBy, setSortBy] = useState("name");
+  const sorted = [...items].sort((a, b) => {
+    if (sortBy === "name") {
+      return a.name.localeCompare(b.name);
+    } else {
+      return a.category.localeCompare(b.category);
+    }
+  });
+  return (
+    <div>
+      <div className="flex flex-col items-center">
+        <h3 className="text-xl">Sort List By:</h3>
+        <div className="flex flex-row justify-center">
+          <button
+            onClick={() => setSortBy("name")}
+            className={`border rounded m-2 px-3 py-1 bg-pink-200 hover:bg-pink-400 dark:hover:bg-blue-400 dark:bg-blue-600 ${sortBy === "name" ? "font-semibold bg-pink-400 dark:bg-blue-900" : ""}`}
+          >
+            Name
+          </button>
+          <button
+            onClick={() => setSortBy("category")}
+            className={`border rounded m-2 px-3 py-1 bg-pink-200 hover:bg-pink-400 dark:hover:bg-blue-400 dark:bg-blue-600 ${sortBy === "category" ? "font-semibold bg-pink-400 dark:bg-blue-900" : ""}`}
+          >
+            Category
+          </button>
+        </div>
+      </div>
+      <ul>
+        {sorted.map((item) => (
+          <Item key={item.id} {...item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
